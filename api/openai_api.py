d54cb8bd5d9e655daf700cd2c8238bcbcdfe016b
KathrynZhu <zhouyaozhu@outlook.com>
2024-03-04 20:30:57 -0600

    able to generate text with select person

--- api/openai_api.py
+++ api/openai_api.py
@@ -6,6 +6,7 @@ import os
 import requests
 import uuid
 
+
 # Assuming you have set OPENAI_API_KEY as an environment variable
 #openai.api_key = os.getenv("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")
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -14,15 +15,29 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 async def generate_text_from_openai(request):
     data = await request.json()
     prompt = data.get("prompt")
+    checked = data.get("selectedCheckboxValues")
+    print("checked here = " + checked[0])
+
+    word = "The speakers are: "
 
     print("in api file" + prompt)
+
+    for name in checked:
+        word += name
+        word += ","
+    word += "."   
+
+    word += prompt
+
+    print("print word here = " + word)
+    
     
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are an assistant."},
-                {"role": "user", "content": prompt}
+                {"role": "user", "content": word}
             ]
         )
         message = response.choices[0].message
